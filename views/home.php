482c4ba1dc4aa73cbe22b25ea66c0da4aa35e000
Chapani Rumeshika Dissanayake <chapirumeshi@gmail.com>
2019-03-03 05:23:23 +0000

    add view button and post it

--- views/home.php
+++ views/home.php
@@ -4,8 +4,20 @@ require_once('template/header.php');?>
 <link rel="stylesheet" href="assets/styles/navbar.css">
 </head>
 <body>
+<button type="button" class="btn btn-primary" style="margin:10px" data-toggle="modal" data-target="#myModal" name="view_btn">
+    View Profile
+  </button>
+  <?php
+   <?php
+    if(isset($_POST['view_btn'])){
+        if(isset($_GET['userid'])){
+                show_userDetails();
+        }
+    }
+    ?>
 <?php
 require_once('template/navigation.php');
+
  
 ?>
 <h1>this is home</h1>
